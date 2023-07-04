be38212bd694341f8be4aa492e36f2312d54cd30
AKVBI <adityaiyer52@gmail.com>
2023-07-04 12:52:34 +0530

    first commit

---
+++ src/test/java/tests/LoginTest2.java
@@ -0,0 +1,28 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.cs.annotations.FrameworkAnnotations;
+
+import DriverManager.DriverManager;
+import pages.HomePage;
+import pages.LoginPage;
+
+public class LoginTest2 extends BaseTest{
+	
+	@FrameworkAnnotations(author = "Katappa",category = "regression")
+	@Test(description="Verify Login Url")
+	public void verifyLoginUrl()
+	{
+		LoginPage loginPage = new LoginPage();
+		loginPage.loginAppl("Admin", "admin123");
+
+		HomePage homePage = new HomePage();
+		String actUrl = homePage.getHomePageUrl();
+		String expUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index";
+		
+		Assert.assertEquals(actUrl, expUrl,"Actual Url:" + actUrl+ " does not match with exp Url: " + expUrl);
+	}
+}
